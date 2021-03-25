934d47cb22855a6c5bc501be915dfed36fadd0c4
eraygundogmus <gundogmuseray@gmail.com>
2021-03-25 12:09:52 +0300

    fetch rest api

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,37 @@
+import faker from "faker"
 
-
-
-export default function Home() {
+export default function Home(posts) {
+  const prod = posts.posts.products
+  console.log(prod)
   return (
     <div className="hello">
       <h1>hello world</h1>
+      <div className="try products">
+        {prod.map((e) => (
+        <div>
+        {e.name}          
+        </div>))}
+      </div>
     </div>
   )
+}
+export async function getStaticProps() {
+
+    const res = await fetch('https://asos2.p.rapidapi.com/products/list?store=2&categoryId=27871&limit=48&offset=0&currency=USD&sizeSchema=US&sort=freshness&lang=en-US&country=US', 
+    {method: 'GET',
+    headers:{
+      "x-rapidapi-key": "104a0a8669mshc29e3cbebbb191bp1aba90jsn839a47e91ece",
+      "x-rapidapi-host": "asos2.p.rapidapi.com",
+      "useQueryString": true
+    },
+  })
+    const posts = await res.json()
+    console.log(posts)
+  // By returning { props: { posts } }, the Blog component
+  // will receive `posts` as a prop at build time
+  return {
+    props: {
+      posts,
+    },
+  }
 }
